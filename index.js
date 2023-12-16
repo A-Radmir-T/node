1373a2ef6690bbc08b47817025d0955f282467ff
a-admir-t <a.radmir.t@gmail.com>
2023-12-16 18:48:33 +0500

    edit Note

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
-const yargs = require("yargs");
-const {addNote, printNotes, removeNote} = require("./nodes.controller");
+const http = require("http");
+const express = require('express')
+const chalk = require("chalk");
+const fs = require('fs/promises')
+const path = require('path')
+const {addNote, getNotes, removeNote, editNote} = require("./nodes.controller");
 
+const port = 3000
 
-yargs.command({
-  command: 'add',
-  describe: 'Add new note list',
-  builder: {
-    title: {
-      type: 'string',
-      describe: 'Note title',
-      demandOption: true
-    }
-  },
-  handler({title}) {
-    addNote(title)
+const basePath = path.join(__dirname, 'pages')
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.set('view engine', 'ejs')
+app.set('views', 'pages')
+
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false
+  })
+})
+app.post('/', async (req, res) => {
+  if (req.body.title) {
+    await addNote(req.body.title)
   }
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: true
+  })
+})
+app.delete('/:id', async (req, res) => {
+
+  await removeNote(req.params.id)
+
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false
+  })
 })
 
-yargs.command({
-  command: 'remove',
-  describe: 'Remove note by id',
-  builder: {
-    id: {
-      type: 'string',
-      describe: 'Note id',
-      demandOption: true
-    }
-  },
-  handler({id}) {
-    removeNote(id)
-  }
-})
+app.put('/:id', async (req, res) => {
 
-yargs.command({
-  command: 'list',
-  describe: 'Print all notes',
-   handler() {
-    printNotes()
-  }
+  await editNote(req.body)
+
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false
+  })
 })
 
 
-yargs.parse()
+
+app.listen(port, () => {
+  console.log(chalk.green(`Server has been started on port ${port}...`))
+})
--- nodes.controller.js
+++ nodes.controller.js
@@ -23,6 +23,18 @@ async function removeNote(id) {
   console.log(chalk.bgRed('The note has been deleted!'))
 }
 
+async function editNote(editedNote) {
+  let notes = await getNotes()
+  notes = notes.map(note => {
+    if (note.id === editedNote.id) {
+      return editedNote
+    }
+    return note
+  })
+  await fs.writeFile('./db.json', JSON.stringify(notes))
+  console.log(chalk.bgBlue('The note has been edited!'))
+}
+
 async function getNotes() {
   const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
   return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
@@ -38,5 +50,5 @@ async function printNotes() {
 }
 
 module.exports = {
-  addNote, printNotes, removeNote
+  addNote, removeNote, getNotes, editNote
 }
